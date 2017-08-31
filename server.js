ad4269c4f0fad3ab84d2e4b4ecef1e70f17eaca6
suraholic <suraholic@gmail.com>
2017-08-31 18:22:09 +0900

    lowdb연결하여 List Page 완성!!

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 
 const express = require('express')
 const app = express()
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+const data = db.get('board').value()[0]
+console.log(data)
+console.log(data.title)
 
 app.set('view engine', 'pug')
 app.set('views', './views');
@@ -8,12 +16,13 @@ app.use('/static', express.static('public'))
 app.locals.pretty = true;
 
 app.get('/', (req,res) => {
-  res.render('index')
+  res.render('index',{data} )
 })
 
 app.get('/write', (req,res)=>{
   res.render('write')
 })
+
 app.listen(3000, ()=>{
-  console.log('Connected to 3000 port...')
+  console.log('server start...')
 })
