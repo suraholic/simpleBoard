fffdf069e5660cf5f9374903c5bbc5fbc1fd1483
suraholic <suraholic@gmail.com>
2017-08-31 20:58:11 +0900

    lowdb-json 글 등록 완료!!

--- public/script.js
+++ public/script.js
@@ -8,8 +8,28 @@ const callIndex = function(){
 const callWrite = function(){
   const listBtn = document.getElementsByClassName('listBtn')[0]
   const sendBtn = document.getElementsByClassName('sendBtn')[0]
+  const frm     = document.getElementById('frm')
+  const title   = document.getElementById('title')
+  const auth    = document.getElementById('auth')
+  const cont    = document.getElementById('cont')
+  const date    = document.getElementById('date')
+  
+  const today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth()+1
+  let yy = today.getFullYear()
+  if(mm<10) { mm = '0'+mm }
+  if(dd<!0) { dd = '0'+dd }
+    
+  listBtn.addEventListener('click',()=>{ location.href = '/' })
+  sendBtn.addEventListener('click', ()=>{
+    if(!title.value){ alert("제목을 입력하세요"); title.focus(); return false}
+    if(!auth.value){ alert("작성자를 입력하세요"); auth.focus(); return false}
+    if(!cont.value){ alert("내용을 입력하세요"); cont.focus(); return false}
 
-  listBtn.addEventListener('click', function(){
-    location.href = '/'
+    date.value = yy + '-' + mm + '-' + dd
+    frm.submit();  
+
   })
 }
+
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
+const app = express()
+
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-const data = db.get('board').value()[0]
-console.log(data)
-console.log(data.title)
+const data = db.get('board').value()
+const bodyParserMiddleware = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'pug')
 app.set('views', './views');
@@ -21,6 +22,26 @@ app.get('/', (req,res) => {
 
 app.get('/write', (req,res)=>{
   res.render('write')
+})
+
+app.post('/iProc', bodyParserMiddleware, (req,res)=>{
+  let idx
+  if(!data.length){
+      idx = 1
+  } else {
+    idx = Math.max(...data.map(item => item.id))+1; 
+  }
+  
+  db.get('board').push({
+    id: idx,
+    title: req.body.title,
+    auth: req.body.auth,
+    cont: req.body.cont,
+    date: req.body.date
+  }).write()
+
+  res.redirect('/')
+ 
 })
 
 app.listen(3000, ()=>{
