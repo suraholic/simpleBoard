a1d7a7775fcd96255a4afa6370d8a07055e26e52
suraholic <suraholic@gmail.com>
2017-09-01 20:27:54 +0900

    관리자 인증 설정 ( express-basic-auth )

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 
 const express = require('express')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
+const basicAuth = require('express-basic-auth')
 
 const app = express()
+
+const authMiddleware = basicAuth({
+  users: { 'admin': '1234' },
+  challenge: true,
+  realm: 'Imb4T3st4pp' 
+})
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -15,11 +23,12 @@ const bodyParserMiddleware = bodyParser.urlencoded({ extended: false })
 app.set('view engine', 'pug')
 app.set('views', './views');
 app.use('/static', express.static('public'))
+app.use(morgan('tiny'))
 app.locals.pretty = true;
 
 app.get('/', (req,res) => { res.render('index',{data} )})
 app.get('/write', (req,res)=>{ res.render('write')})
-app.get('/admin', (req,res)=>{ res.render('admin', {data} )})
+app.get('/admin', authMiddleware, (req,res)=>{ res.render('admin', {data} )})
 
 app.get('/read/:id', (req,res) => {
   const id = req.params.id
